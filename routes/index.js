0f9189fe365e4485748c2ce03871df0ca7b36608
鈴木結衣 <suzukiyui@suzukiyuinoMacBook-Air.local>
2025-12-10 07:20:27 +0900

    Change root to redirect to login page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
+// routes/index.js
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// ルートに来たらログインページに飛ばす
+router.get('/', function (req, res) {
+  res.redirect('/login');
 });
 
 module.exports = router;
